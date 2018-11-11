a8a44b97e35dffc983273ac607a96cefc56becf6
patti mint <patseade@Pats-MacBook-Pro.local>
2018-11-10 19:48:42 -0600

    final3

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,13 @@
 const mysql = require("mysql");
 
+var connection;
+
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
 
 
-let connection = mysql.createConnection({
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
@@ -13,9 +15,9 @@ let connection = mysql.createConnection({
   database: "burgers_db"
 
 
-});
-};
+})
 
+};
 
 connection.connect(function(err) {
     if (err) {
@@ -25,6 +27,7 @@ connection.connect(function(err) {
   
     console.log("connected as id " + connection.threadId);
   });
-  
+
+
 
 module.exports = connection;
